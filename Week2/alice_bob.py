00ba324715bf3980b543facd3d54d84c3c7374ee
XolisaCodes <69010686+XolisaCodes@users.noreply.github.com>
2020-07-31 21:57:48 +0200

    Update alice_bob.py

---
+++ Week2/alice_bob.py
@@ -0,0 +1,5 @@
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+aliceScore = sum([(1 if a[i] > b[i] else 0) for i in range(3)])
+bobScore =  sum([(1 if a[i] < b[i] else 0) for i in range(3)])
+print(aliceScore, bobScore)
