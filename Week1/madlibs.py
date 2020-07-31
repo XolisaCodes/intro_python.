4cd8033a03a1cb0a8272519ad95f124ac09d9174
XolisaCodes <69010686+XolisaCodes@users.noreply.github.com>
2020-07-31 21:30:19 +0200

    Update madlibs.py

---
+++ Week1/madlibs.py
@@ -0,0 +1,7 @@
+proffession = input("Enter a proffession: ")
+Salary_figure = input("Enter a salary figure: ")
+Goal = input("Enter your Goal: ")
+
+print("Zaio produces " + proffession)
+print(Salary_figure + " will be my starting salary")
+print("I am ready to "  + Goal)
